b9bcc18903a8deb7ed4de685e4c1c107dddf8ccf
NV Binh <95974734+NVB07@users.noreply.github.com>
2023-05-18 02:55:34 +0700

    update clock

--- Clock/Data/main.js
+++ Clock/Data/main.js
@@ -1,45 +1,56 @@
-const $  =document.querySelector.bind(document);
+const $ = document.querySelector.bind(document);
 
-var time;
-var hour;
-var minute;
-var second;
+let time;
+let hour;
+let minute;
+let second;
+let day;
+let month;
+let year;
 
-var clockHour  = $(".hours")
-var clockMinutes  = $(".minutes")
-var clockSecons = $(".secons")
-var digitalClock = $(".clock-digital")
+let clockHour = $(".hours");
+let clockMinutes = $(".minutes");
+let clockSecons = $(".secons");
+let digitalClock = $(".clock-digital");
+let fullDay = $(".day");
 
-function clock(){
+function clock() {
     time = new Date();
     hour = time.getHours();
     minute = time.getMinutes();
     second = time.getSeconds();
     hour = hour % 12;
 
-    var SeconDeg = second*6;
-    var MinuteDeg = minute*6 + SeconDeg/60;
-    var HourDeg = hour*30 + minute*30/60;
-    clockHour.style.transform = "rotate("+HourDeg+"deg)"
-    clockMinutes.style.transform = "rotate("+MinuteDeg+"deg)";
-    clockSecons.style.transform = "rotate("+SeconDeg+"deg)"
+    let SeconDeg = second * 6;
+    let MinuteDeg = minute * 6 + SeconDeg / 60;
+    let HourDeg = hour * 30 + (minute * 30) / 60;
+    clockHour.style.transform = "rotate(" + HourDeg + "deg)";
+    clockMinutes.style.transform = "rotate(" + MinuteDeg + "deg)";
+    clockSecons.style.transform = "rotate(" + SeconDeg + "deg)";
 }
-function hvn(){
+function hvn() {
     time = new Date();
+
+    day = time.getDate();
+    month = time.getMonth() + 1;
+    year = time.getFullYear();
     hour = time.getHours();
     minute = time.getMinutes();
     second = time.getSeconds();
-    if(hour < 10) {
+    console.log(day, month, year);
+    if (hour < 10) {
         hour = "0" + hour;
     }
-    if(minute < 10) {
+    if (minute < 10) {
         minute = "0" + minute;
     }
-    if(second < 10) {
+    if (second < 10) {
         second = "0" + second;
     }
-    var now = hour + " : " + minute + " : " + second;   
+    let now = hour + " : " + minute + " : " + second;
+    let dayMonthYear = `${day} / ${month} / ${year}`;
     digitalClock.innerHTML = now;
-    clock()
-}                                                                
-setInterval("hvn()",1000);
+    fullDay.innerHTML = dayMonthYear;
+    clock();
+}
+setInterval("hvn()", 1000);
